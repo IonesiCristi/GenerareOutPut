a5f87b388171e72d7653965f50f776fe09578cf5
IonesiCristi <ionesicristi@gmail.com>
2017-01-08 23:35:33 +0200

    little things

--- main.py
+++ main.py
@@ -17,17 +17,17 @@ def generateOutPut(sentence):
 
     randIncurajare = random.randint(0,2)
 
-    randAlter = random.randint(5,7)
+    #randAlter = random.randint(5,7)
 
     if randIncurajare == 1:
         newSentence = incurajatori.creeazaSintagmaMagulire(newSentence)
 
-    for i in range(0,15):
+    for i in range(0,3):
        oldSentence = alter.alter(newSentence, 1, 2)
        newSentence = oldSentence
        #print newSentence
-    #newSentence = informal.useContractions(newSentence)
+    newSentence = informal.useContractions(newSentence)
 
     return newSentence
 
-print generateOutPut("I like cats.")
+print(generateOutPut("You seem to be a very interesting person. Tell me more about yourself. What animal best represents you and why? "))
